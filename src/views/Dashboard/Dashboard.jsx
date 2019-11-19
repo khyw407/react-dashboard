9e0c9bf234cc94c1979c0e284de19983606a4808
Hyunwoo Kim <khyw407@gmail.com>
2019-11-19 16:43:18 +0900

    대시보드 템플릿 추가:

--- src/views/Dashboard/Dashboard.jsx
+++ src/views/Dashboard/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 import {
   Controller
@@ -17,79 +18,110 @@ const Dashboard = () => {
 
   return (
     <div className={classes.root}>
+      <Typography variant="h6">Kubernetes Cluster</Typography>
       <Grid
         container
-        spacing={4}
+        spacing={1}
       >
         <Grid
           item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={3}
-          sm={6}
-          xl={3}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={8}
-          md={12}
-          xl={9}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
+          xs={1}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={4}
-          md={6}
-          xl={3}
-          xs={12}
+          xs={3}
         >
           <Controller />
         </Grid>
         <Grid
           item
-          lg={8}
-          md={12}
-          xl={9}
-          xs={12}
+          xs={3}
+        >
+          <Controller />
+        </Grid>
+      </Grid>
+      <Typography variant="h6">Kubernetes Workloads</Typography>
+      <Grid
+        container
+        spacing={1}
+      >
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={1}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={3}
+        >
+          <Controller />
+        </Grid>
+        <Grid
+          item
+          xs={3}
         >
           <Controller />
         </Grid>

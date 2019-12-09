d7131fcc66a34e958532cec7bd9795e19d78b5cf
khyw407 <khyw407@gmail.com>
2019-12-09 15:58:23 +0900

    Pod template 추가

---
+++ src/views/Kubernetes/components/Pod/components/List/List.jsx
@@ -0,0 +1,208 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import PerfectScrollbar from 'react-perfect-scrollbar';
+import { makeStyles } from '@material-ui/styles';
+import {
+  Card, CardContent, Grid, Typography, Divider,
+  Table, TableBody, TableCell, TableRow, ListItemIcon, ListItemText, colors, TableHead
+} from '@material-ui/core';
+import mockData from './data';
+
+const useStyles = makeStyles(theme => ({
+  root: {height: '100%'},
+  item: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    fontWeight: 700
+  },
+  divider: {
+    margin: theme.spacing(2, 0)
+  },
+  inner: {
+    minWidth: 300
+  },
+  tableCell: {
+    fontWeight: 900,
+    fontSize: 'large',
+  },
+  critical: {
+    '& $indicator': {
+      borderColor: colors.red[600]
+    }
+  },
+  tableHead: {
+    backgroundColor: '#fafafa'
+  },
+  indicator: {
+    height: 12,
+    width: 12,
+    borderWidth: 4,
+    borderStyle: 'solid',
+    borderColor: colors.cyan[500],
+    borderRadius: '50%'
+  },
+  listItemText: {
+    marginRight: theme.spacing(1)
+  }
+}));
+
+const List = props => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  const [list] = useState(mockData);
+
+  return (
+    <Card
+      {...rest}
+      className={clsx(classes.root, className)}
+    >
+      <CardContent>
+          <Grid 
+            container
+            justify="space-between"
+          >
+            <Grid item>
+                <Typography
+                    className={classes.title}
+                    color="textSecondary"
+                    gutterBottom
+                    variant="h6"
+                >
+                    List
+                </Typography>
+            </Grid>
+          </Grid>
+          <Divider className={classes.divider} />
+          <PerfectScrollbar options={{ suppressScrollY: true }}>
+            <div className={classes.inner} style={{ overflow: 'auto', height: '350px' }}>
+                <Table>
+                    <TableHead className={classes.tableHead}>
+                        <TableRow>
+                            <TableCell className={classes.tableCell}></TableCell>
+                            <TableCell className={classes.tableCell}>Name</TableCell>
+                            <TableCell className={classes.tableCell}>Host IP</TableCell>
+                            <TableCell className={classes.tableCell}>Namespace</TableCell>
+                            <TableCell className={classes.tableCell}>Kind</TableCell>
+                            <TableCell className={classes.tableCell}>CPU(%)</TableCell>
+                            <TableCell className={classes.tableCell}>Memory(%)</TableCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {list.map((item, i) => (
+                          <TableRow key={item.id}>
+                            <TableCell>
+                              <ListItemIcon>
+                                  <span className={classes.indicator} />
+                              </ListItemIcon>
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.name}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.hostIP}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.namespace}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.kind}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.cpu}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                            <TableCell>
+                              <ListItemText
+                                  className={classes.listItemText}
+                                  primary={
+                                    <>
+                                      <Typography
+                                        variant="subtitle2"
+                                        color="textPrimary"
+                                      >
+                                        <strong>{item.memory}</strong>
+                                      </Typography>
+                                    </>
+                                  }
+                              />
+                            </TableCell>
+                          </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+            </PerfectScrollbar>
+      </CardContent>
+    </Card>
+  );
+};
+
+List.propTypes = {
+  className: PropTypes.string
+};
+
+export default List;
---
+++ src/views/Kubernetes/components/Pod/components/List/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        id: 1,
+        name: 'terrifying-eel-nginx-ingress-controller-7cbbb8844-vmf9z',
+        hostIP: '10.0.0.18',
+        namespace: 'default',
+        kind: 'ReplicaSet',
+        cpu: '0.4',
+        memory: '0.435'
+    },
+    {
+        id: 2,
+        name: 'terrifying-eel-nginx-ingress-controller-default-backend-fcbqq78',
+        hostIP: '10.0.0.19',
+        namespace: 'default',
+        kind: 'ReplicaSet',
+        cpu: '0',
+        memory: '0.019'
+    },
+    {
+        id: 3,
+        name: 'cert-manager-cert-manager-77558cbf86-zxzz7',
+        hostIP: '10.0.0.20',
+        namespace: 'kube-system',
+        kind: 'ReplicaSet',
+        cpu: '0.6',
+        memory: '0.068'
+    },
+    {
+        id: 4,
+        name: 'kube-dns-77f747b66b-8c744',
+        hostIP: '10.0.0.18',
+        namespace: 'kube-system',
+        kind: 'ReplicaSet',
+        cpu: '0.2',
+        memory: '0.049'
+    }
+];

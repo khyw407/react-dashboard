2d36e5384a0d2053e09c409e0f61c95153c1e21e
khyw407 <khyw407@gmail.com>
2019-12-09 15:28:15 +0900

    deployment template 추가

---
+++ src/views/Kubernetes/components/Deployment/Deployment.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { Grid } from '@material-ui/core';
+import {
+  Cpu, List, Memory
+} from './components';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+const Cluster = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={1}
+      >
+        <Grid
+          item
+          lg={6}
+          sm={12}
+          xl={6}
+          xs={12}
+        >
+          <Cpu />
+        </Grid>
+        <Grid
+          item
+          lg={6}
+          sm={12}
+          xl={6}
+          xs={12}
+        >
+          <Memory />
+        </Grid>
+        <Grid
+          item
+          lg={12}
+          sm={12}
+          xl={12}
+          xs={12}
+        >
+          <List />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Cluster;
